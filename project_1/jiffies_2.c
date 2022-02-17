63f8062f9d78fb25474d14a7c87af0d957432c89
Mick Vespa <79177297+MickVespa@users.noreply.github.com>
2022-02-17 14:33:10 -0500

    Update jiffies_2.c

--- project_1/jiffies_2.c
+++ project_1/jiffies_2.c
@@ -1,57 +1,66 @@
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
+#include<linux/init.h>
+#include<linux/module.h>
+#include <linux/uaccess.h>
+#include<linux/fs.h>
+#include<linux/sched.h>
+#include<linux/device.h>
+#include<linux/slab.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h>
-#include <asm/param.h>
+#include <linux/string.h>
+#include <linux/timer.h>
 
-#define BUFFER_SIZE 128
-#define PROC_NAME "jiffies"
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR(" Auther Name ");
+MODULE_DESCRIPTION(" Test Driver Module ");
 
+static int myInit(void);
+static void myExit(void);
 
-ssize_t proc_read(struct file *file, char __user *usr_buf,size_t count, loff_t *pos);
+struct proc_dir_entry *my_proc;
 
-static struct file_operations proc_ops ={
-  .owner = THIS_MODULE,
-  .read = proc_read,
+/* Read operation on proc */
+ssize_t read_data(struct file *fp, char *buf, size_t len, loff_t * off)
+{
+/* Logic to read data once */
+static int finished=0;
+if(finished) {
+finished=0;
+return 0;
+}
+finished=1;
+
+/* find out current value of jiffies */
+
+sprintf(buf, "Value of jiffies %lu \n", jiffies);  
+return strlen(buf);
+}
+
+/* File operation on proc */
+static struct file_operations fops = {
+.owner=THIS_MODULE,
+.read=read_data,
 };
 
-/* This function is called when the module is loaded. */
-int proc_init(void){
-
-/* creates the /proc/jiffies entry */
-  proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-  return 0;
-}
-
-/* This function is called when the module is removed. */
-void proc_exit(void){
-
-/* removes the /proc/jiffies entry */
-  remove_proc_entry(PROC_NAME, NULL);
-}
-
-/* This function is called each time /proc/jiffies is read */
-ssize_t proc_read(struct file *file, char  __user *usr_buf,size_t count, loff_t *pos)
+/* Init of kernel module */
+static int __init myInit(void)
 {
-  int rv = 0;
-  char buffer[BUFFER_SIZE];
-  static int completed = 0;
 
-  if (completed)
-  {
-  completed = 0;
-  return 0;
-  }
-  completed = 1;
-  rv = sprintf(buffer, "JIFFIES : %lu \n",jiffies);
-  /* copies kernel space buffer to user space usrbuf */
-  raw_copy_to_user(usr_buf, buffer, rv);
-  return rv;
+/* Create proc file with name of jiffies */
+my_proc = proc_create("jiffies", 0666, NULL, &fops);
+
+if(my_proc == NULL){
+printk(KERN_INFO "Error to create proc File\n");
+return -1;
 }
 
-module_init(proc_init);
-module_exit(proc_exit);
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("JIFFIES MODULE");
-MODULE_AUTHOR("SGG");
+return 0;
+}
+/* exit of kernel module */
+static void myExit(void)
+{
+remove_proc_entry("jiffies",NULL);
+return;
+}
+
+module_init(myInit);
+module_exit(myExit);
